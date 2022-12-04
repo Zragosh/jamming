fb7033f1bb63d0615c98340acfb4af1d003ed440
Dragosh <81014754+Zragosh@users.noreply.github.com>
2022-12-04 17:08:57 +0200

    create App component

---
+++ src/Components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+// import SearchBar component
+// import SearchResult component
+// import Playlist component
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className='highlight'>mmm</span>ing</h1>
+        <div className='App'>
+          {/* SearchBar component */}
+            <div className='App-playlist'>
+              {/* SearchResult component */}
+              {/* Playlist component */}
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
