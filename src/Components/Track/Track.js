fc7b1d85728448ef5c3cb69b67b6393d917bd992
Dragosh <81014754+Zragosh@users.noreply.github.com>
2022-12-04 18:30:50 +0200

    create Track component

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+
+  renderAction() {
+    let buttonType;
+    if (this.props.isRemoval) {
+      buttonType = '+';
+    } else {
+      buttonType = '-';
+    }
+
+    return <button className='Track-action'>{buttonType}</button>;
+  }
+
+  render() {
+    return (
+      <div className='Track'>
+        <div className='Track-information'>
+          {/* <h3>track name</h3> */}
+          {/* <p>track artist | track album</p> */}
+        </div>
+        {this.renderAction()}
+      </div>
+    );
+  } 
+}
+
+export default Track;
