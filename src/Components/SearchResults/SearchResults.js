dc234286cd7abeb978d00e54eec477dac8db5e1c
Dragosh <81014754+Zragosh@users.noreply.github.com>
2022-12-04 17:34:36 +0200

    create SearchResults component

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SearchResults.css';
+// import TrackList component
+
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className='SearchResults'>
+        <h2>Results</h2>
+        {/* TrackList component */}
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
