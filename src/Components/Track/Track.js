8be0998c2e045ceee89e9ef30766ffe6c5405115
Dragosh <81014754+Zragosh@users.noreply.github.com>
2022-12-06 08:39:10 +0200

    code format changes

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Track.css';
 
 export class Track extends React.Component {
-  
+
   renderAction() {
     let buttonType;
     if (this.props.isRemoval) {
@@ -18,8 +18,8 @@ export class Track extends React.Component {
     return (
       <div className='Track'>
         <div className='Track-information'>
-          {/* <h3>track name</h3> */}
-          {/* <p>track artist | track album</p> */}
+          <h3>track name</h3>
+          <p>track artist | track album</p>
         </div>
         {this.renderAction()}
       </div>
