26c2747ef86826616e4bacef6ad1a99eb6076724
Dragosh <81014754+Zragosh@users.noreply.github.com>
2022-12-06 12:12:00 +0200

    pass tracks props from Playlist to TrackList

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Playlist.css';
-// import { TrackList } from '../TrackList/TrackList';
+import { TrackList } from '../TrackList/TrackList';
 
 export class Playlist extends React.Component {
   
@@ -8,7 +8,7 @@ export class Playlist extends React.Component {
     return (
       <div className='Playlist'>
         <input defaultValue={'New Playlist'} />
-        {/* <TrackList /> */}
+        <TrackList tracks={this.props.playlistTracks} />
         <button className='Playlist-save'>SAVE TO SPOTIFY</button>
       </div>
     );
