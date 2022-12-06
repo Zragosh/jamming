def908f91f214450748a485d14aa97c5f86ed641
Dragosh <81014754+Zragosh@users.noreply.github.com>
2022-12-06 11:56:55 +0200

    finalize Tracklist + Track components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,6 +7,7 @@ import { Playlist } from '../Playlist/Playlist';
 export class App extends React.Component {
   constructor(props) {
     super(props);
+    
     this.state = {
       searchResults: [
         { name: "name1", artist: "artist1", album: "album1", id: 1 },
