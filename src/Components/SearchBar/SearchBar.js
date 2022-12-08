3973e825ec7cde94c55efd6129934f8abf8b4538
Dragosh <81014754+Zragosh@users.noreply.github.com>
2022-12-08 11:18:49 +0200

    add state to SearchBar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,6 +5,8 @@ export class SearchBar extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {term: ''};
+
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
