6fcb8bb83e9d6b8142fe38411ce5233c747102f0
Dragosh <81014754+Zragosh@users.noreply.github.com>
2022-12-08 13:02:33 +0200

    create Sporify module to get user access token

---
+++ src/util/Spotify.js
@@ -0,0 +1,28 @@
+const clientID = '6ee054486c6e433a80088de12a7b570d';
+const redirectUri = 'http://localhost:3000/';
+let accesToken;
+
+export const Spotify = {
+  getAccessToken() {
+    if (accesToken) {
+      return accesToken;
+    }
+
+    // check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accesToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      // this clears the paremeters, allowing to grab a new access token when it expires
+      window.setTimeout(() => accesToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accesToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}
+`;
+      window.location = accessUrl;
+    }
+  }
+};
