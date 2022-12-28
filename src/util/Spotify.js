8aa59210a81b32330be3f77374b22695ae1703ae
Dragosh <81014754+Zragosh@users.noreply.github.com>
2022-12-28 14:17:42 +0200

    add API

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -30,10 +30,10 @@ export const Spotify = {
     const accessToken = Spotify.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, 
     { headers: {
-      Authorization: `Bearer ${accessToken}`
-    }}).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
+      Authorization: `Bearer ${accessToken}`}}
+      ).then(response => {
+      return response.json()}
+      ).then(jsonResponse => {
       if (!jsonResponse) {
         return [];
       }
@@ -45,5 +45,38 @@ export const Spotify = {
         uri: track.uri
       }));
     });
+  },
+
+  savePlaylist(name, trackUris) {
+    if (!name || !trackUris.length) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}`};
+    let userId;
+
+    return fetch('https://api.spotify.com/v1/me', { headers: headers }
+    ).then(response => response.json()
+    ).then(jsonResponse => {
+      userId = jsonResponse.id;
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,
+      {
+        headers: headers,
+        method: 'POST',
+        body: JSON.stringify({ name: name })
+      }).then(response => response.json()
+      ).then(jsonResponse => {
+        const playlistId = jsonResponse.id;
+        return fetch(
+          `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+          { 
+            headers: headers,
+            method: 'POST',
+            body: JSON.stringify({ uris: trackUris })
+          }
+        );
+      })
+    })
   }
 };
